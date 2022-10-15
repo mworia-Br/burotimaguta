34782083df53ea7b03549f72f0c56e0826d2741f
Brian Nganga <66476363+Braygithub@users.noreply.github.com>
2022-10-15 20:27:54 +0300

    registered models is one per register call

--- buyers/admin.py
+++ buyers/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import BuyerInfo, BuyerPayments
 
-admin.site.register(BuyerInfo,BuyerPayments)
+admin.site.register(BuyerInfo)
+admin.site.register(BuyerPayments)
