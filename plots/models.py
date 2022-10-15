9c1f1c69c45d96961f6225d9873689056c46986b
Brian Nganga <66476363+Braygithub@users.noreply.github.com>
2022-10-15 15:07:41 +0300

    class plot

--- plots/models.py
+++ plots/models.py
@@ -11,3 +11,12 @@ plotsize_choices= (
 class Plot(models.Model):
     projectName=models.CharField(max_length=100)
     seller=models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+    description=models.TextField()
+    availableUnits=models.IntegerField(max_length=4)
+    plotsize=models.CharField(max_length=30,choices=plotsize_choices,default="")
+    nearByamenities=models.TextField()
+    images=models.FileField(upload_to='plots/images/')
+    sellingprice=models.IntegerField(max_length=20,null=True)
+    discountprice=models.IntegerField(max_length=20,null=True)
+    #userbooked=models.ManyToManyField(User,blank=True)
