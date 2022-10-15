fab031776f2c63c5f015c02c3a5a19644c008353
Brian Nganga <66476363+Braygithub@users.noreply.github.com>
2022-10-15 16:51:28 +0300

    added buyer models

--- buyers/models.py
+++ buyers/models.py
@@ -23,3 +23,25 @@ class BuyerModel(models.Model):
         ("Deposit + 24months","Deposit + 24months")
         ("OT","OT")
     )
+    first_name = models.CharField(max_length=255, null=True)
+    middle_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_OPTIONS, null=True)
+    residential_status = models.CharField(max_length=2, choices=RESIDENTIAL_OPTIONS, null=True)
+    current_address = models.CharField(max_length=100, null=True)
+    national_id = models.CharField(max_length=10, unique=True, null=True)
+    date_of_birth = models.DateField(null=True)
+    kra_pin = models.CharField(max_length=11, unique=True, null=True)
+    passport_photo = models.ImageField(blank=True, null=True)
+    buyer_personal_number = models.CharField(max_length=50, null=True)
+    #registeredBy
+    buyers_email = models.EmailField(unique=True, null=True)
+    mobile_phone = models.CharField(max_length=15, null=True)
+    project_assigned = models.ManyToManyField('Plot', blank=True)
+    date_of_registration = models.DateField(null=True)
+    contract_type = models.CharField(choices=PAYMENT_CONTRACT, max_length=1, null=True)
+    deposit_amount = models.CharField(max_length=15, null=True)
+    balance_amount = models.CharField(max_length=15, null=True)
+
+    def __str__(self):
+        return self.last_name
