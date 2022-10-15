441fd51aa6e792a6e98219e9bc73e24f5e7dac17
Brian Nganga <66476363+Braygithub@users.noreply.github.com>
2022-10-15 15:52:14 +0300

    buyer models

--- buyers/models.py
+++ buyers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from plots.models import Plot
+
+
+class BuyerModel(models.Model):
+    GENDER_OPTIONS =(
+        ("M","Male")
+        ("F","Female")
+        ("O","Other")
+    )
+
+    RESIDENTIAL_OPTIONS = (
+        ('R','Resident')
+        ("NR","Non-Resident")
+    )
+
+    PAYMENT_CONTRACT = (
+        ()
+    )
