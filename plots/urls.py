6004301e637f39d39dcb2aace83f66bba4b776e7
Brian Nganga <66476363+Braygithub@users.noreply.github.com>
2022-10-18 11:27:05 +0300

    added url paths

---
+++ plots/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from plots import views
+
+app_name="plots"
+urlpatterns = [
+    path('plots/', views.plots, name='plots'),
+    path('plots/<int:plot_id>/', views.plot_detail, name='plot_detail'),
+    path('plotnumbers/', views.all_plotnumbers, name='plotnumbers'),
+    path('plotnumbers/<int:plotnumber_id>/', views.plotnumber_detail, name='plotnumber_detail')   
+]
