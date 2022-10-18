0dccd1c3cbf8d57278effd846baaad7410126d46
Brian Nganga <66476363+Braygithub@users.noreply.github.com>
2022-10-18 13:33:04 +0300

    done

--- burotimaguta/urls.py
+++ burotimaguta/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('buyers.urls')),
+    path('', include('plots.urls'))
 ]
 #if settings.DEBUG:
 #    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
