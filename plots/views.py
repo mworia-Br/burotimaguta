51417b69b20b6a47d66bcb9fb810884d6808e51f
Brian Nganga <66476363+Braygithub@users.noreply.github.com>
2022-10-18 10:28:59 +0300

    imports done

--- plots/views.py
+++ plots/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirects
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from plots.models import Plot, Plot_Number
+
