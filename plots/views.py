0e04e3f3cdb0af37812d77e2c80c4084436c748f
Brian Nganga <66476363+Braygithub@users.noreply.github.com>
2022-10-18 11:12:49 +0300

    Created plot & plotnumber views

--- plots/views.py
+++ plots/views.py
@@ -5,3 +5,31 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from plots.models import Plot, Plot_Number
 
+def plots(request):
+    plots = Plot.objects.all()
+    """
+    if request.method == 'POST':
+        form = plotsForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "plot added booking successfuly initiated")
+        else:
+            messages.error(request, "Error adding plot")
+    else:
+        form = plotsForm()
+    """    
+    return render(request, 'plots/plots.html', {'plots':plots,'plots_form': form})
+
+def plot_detail(request, plot_id):
+    plot = get_object_or_404(Plot, pk=plot_id)
+    return render(request, 'plots/plot_detail.html', {'plot': plot})
+
+def all_plotnumbers(request):
+    plotnumbers = Plot_Number.objects.all()
+
+    return render(request, 'plots/plotnumbers.html', {'plotnumbers': plotnumbers})
+
+def plotnumber_detail(request, plotnumber_id):
+    plotnumber = get_object_or_404(Plot_Number, pk=plotnumber_id)
+    return render(request, 'plotnumbers/plotnumber_detail.html', {'plotnumber': plotnumber})
